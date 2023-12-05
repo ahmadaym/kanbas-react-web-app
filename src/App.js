1e04258adf6e87ffd591271882ec48273b045848
Ayman Ahmad <ahmad.ay@northeastern.edu>
2023-12-04 19:00:26 -0500

    A6 Commit

--- src/App.js
+++ src/App.js
@@ -5,35 +5,22 @@ import Kanbas from "./Kanbas";
 import HelloWorld from "./Labs/a3/HelloWorld";
 import Labs from "./Labs/index";
 import { Routes, Route, Navigate } from "react-router";
+import Project from "./Project";
 
 function App() {
   return (
     <HashRouter>
       <div>
         <Routes>
-          <Route path="/" element={<Navigate to="/Labs" />} />
+          <Route path="/" element={<Navigate to="/project" />} />
+          <Route path="/project/*" element={<Project />} />
+          {/* <Route path="/" element={<Navigate to="/Labs" />} /> */}
           <Route path="/hello" element={<HelloWorld />} />
           <Route path="/Labs/*" element={<Labs />} />
           <Route path="/Kanbas/*" element={<Kanbas />} />
         </Routes>
       </div>
     </HashRouter>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
